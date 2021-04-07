a6d95e8e9de822da7c5a477c8902d24fa473e97e
minlwin <lwin.zawmin@gmail.com>
2021-04-07 15:16:24 +0630

    Commit Balance Demo Project

--- api-demo/balance-demo/src/app/balance/details/details.component.ts
+++ api-demo/balance-demo/src/app/balance/details/details.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BalanceService } from 'src/app/services/balance.service';
 
 @Component({
   templateUrl: './details.component.html',
   styles: [
   ]
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent {
 
-  constructor() { }
+  balance: any
+  details: any[] = []
 
-  ngOnInit(): void {
+  constructor(route: ActivatedRoute, service: BalanceService) {
+    route.params.subscribe(params => {
+      const id = Number(params['id'])
+
+      if (id) {
+        service.findById(id).subscribe(data => this.balance = data)
+        service.getDetails(id).subscribe(data => this.details = data)
+      }
+    })
   }
 
 }
