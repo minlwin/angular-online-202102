1a2037580ad4a2c700d5871d32d51463285dbb28
minlwin <lwin.zawmin@gmail.com>
2021-05-08 01:14:35 +0630

    Update 20210508

---
+++ angular/se03-navigation/projects/hello/src/app/simple/simple.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './simple.component.html',
+  styles: [
+  ]
+})
+export class SimpleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
